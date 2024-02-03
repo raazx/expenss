955550d1818c3b7d07f9f3d8ecaae5c1ec1b337c
raazx <raazx60@gmail.com>
2024-02-03 17:01:23 +0200

    add appBar

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -1,8 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();
 
 enum Categoory { food, travel, leisure, work, health, other }
+
+final formatter = DateFormat.yMd();
+
+final categoryIcons = {
+  Categoory.food: Icons.restaurant_menu,
+  Categoory.travel: Icons.flight_takeoff,
+  Categoory.leisure: Icons.local_play,
+  Categoory.work: Icons.business,
+  Categoory.health: Icons.local_hospital,
+  Categoory.other: Icons.devices_other,
+};
 
 class Expense {
   Expense({
@@ -16,4 +29,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Categoory category;
+
+  String get formatedDate {
+    return formatter.format(date);
+  }
 }
--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -8,11 +8,27 @@ class ExpenseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text(
-          expense.title,
-        ),
-      ),
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            children: [
+              Text(expense.title),
+              SizedBox(height: 4),
+              Row(
+                children: [
+                  Text('\$ ${expense.amount.toStringAsFixed(2)}'),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      Icon(categoryIcons[expense.category]),
+                      const SizedBox(width: 8),
+                      Text(expense.formatedDate)
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
     );
   }
 }
